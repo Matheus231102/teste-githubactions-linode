e5e891af022b9a3d09f752b275a97ffe32dc29d8
Matheus Badia <matheusbadia23@gmail.com>
2025-06-08 14:49:14 -0300

    add plugin para verificar .properties do pom.xml

--- src/main/java/com/desenv/mavenproject1/resources/JakartaEE10Resource.java
+++ src/main/java/com/desenv/mavenproject1/resources/JakartaEE10Resource.java
@@ -5,6 +5,8 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  *
@@ -15,8 +17,21 @@ public class JakartaEE10Resource {
     
     @GET
     public Response ping() throws IOException{
-        return Response
-                .ok(Json.createObjectBuilder().add("teste", "1").build().toString())
-                .build();
+        Properties props = new Properties();
+        try (InputStream input = JakartaEE10Resource.class.getResourceAsStream("/project.properties")) {
+            props.load(input);
+            String version = props.getProperty("project.version", "não foi possível pegar a versão disponível");
+            return Response
+                    .status(Response.Status.OK)
+                    .entity(Json.createObjectBuilder().add("project.version", version).build().toString())
+                    .build();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response
+                    .status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity(Json.createObjectBuilder().add("exception", e.getMessage()))
+                    .build();
+        }
     }
 }
